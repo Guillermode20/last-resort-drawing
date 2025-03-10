5b0317319fb6bb97967207a7bca9a40e6d4338cf
Will Hick <william_hick@hotmail.co.uk>
2025-03-10 12:23:59 +0000

    Enhance startup script with error handling for virtual environment creation and activation

--- startup.sh
+++ startup.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-python3 -m venv venv
-source .venv/bin/activate
+python3 -m venv .venv || { echo "Failed to create virtual environment"; exit 1; }
+
+# Check if the virtual environment was created successfully
+if [ -f ".venv/bin/activate" ]; then
+  source .venv/bin/activate
+else
+  echo "Virtual environment activation script not found.  Exiting."
+  exit 1
+fi
+
 cd server
-pip install -r requirements.txt
+pip install --no-cache-dir -r requirements.txt
+
 uvicorn main:app --host 0.0.0.0 --port 8080
